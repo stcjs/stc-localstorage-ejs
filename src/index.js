fe46c2d19840ae47406c107b7155fccb10f416ff
JerryQu <quguangyu@gmail.com>
2016-07-22 17:07:23 +0800

    no message

--- src/index.js
+++ src/index.js
@@ -5,70 +5,72 @@ export default class stcAdapter {
   }
 
   getLsSupportCode() {
-    let { ld, rd } = this.config.tpl;
+    let ld = this.config.tpl.ld[0];
+    let rd = this.config.tpl.rd[0];
+
     let nlsCookie = this.options.nlsCookie;
 
     let data = {};
 
-    data['if'] = `${ld}if isset($smarty.server.HTTP_USER_AGENT) && strpos($smarty.server.HTTP_USER_AGENT, "MSIE ") === false && !isset($smarty.cookies.${nlsCookie})${rd}`;
-    data['else'] = `${ld}else${rd}`;
-    data['end'] = `${ld}/if${rd}`;
+    data['if'] = `${ld} if(http.userAgent().indexOf("MSIE ") === -1 && !http.cookie("${nlsCookie}")) { ${rd}`;
+    data['else'] = `${ld} } else { ${rd}`;
+    data['end'] = `${ld} } ${rd}`;
 
     return data;
   }
 
   getLsConfigCode(appConfig) {
-    let { ld, rd } = this.config.tpl;
+    let ld = this.config.tpl.ld[0];
+    let rd = this.config.tpl.rd[0];
     
     let configStr = JSON.stringify(appConfig);
 
-    return `${ld}$stc_ls_config=json_decode("${configStr}", true)${rd}`;
+    return `${ld} var stc_ls_config = JSON.parse(\'${configStr}\') ${rd}`;
   }
 
   getLsBaseCode() {
-    let { ld, rd } = this.config.tpl;
+    let ld = this.config.tpl.ld[0];
+    let rd = this.config.tpl.rd[0];
+
     let name = 'stc_ls_base_flag';
 
     let data = {};
 
-    data['if'] = `${ld}if !isset($${name})${rd}${ld}$${name}=true${rd}`;
-    data['end'] = `${ld}/if${rd}`;
+    data['if'] = `${ld} if(!http.${name}) { http.${name}=true; ${rd}`;
+    data['end'] = `${ld} } ${rd}`;
 
     return data;
   }
 
   getLsParseCookieCode() {
-    let { ld, rd } = this.config.tpl;
+    let ld = this.config.tpl.ld[0];
+    let rd = this.config.tpl.rd[0];
+
     let lsCookie = this.options.lsCookie;
 
     let content = [
-      `${ld}if isset($smarty.cookies.${lsCookie}) ${rd}`, 
-      `${ld}$stc_ls_cookie=$smarty.cookies.${lsCookie}${rd}`, 
-      `${ld}else${rd}`, 
-      `${ld}$stc_ls_cookie=""${rd}`, 
-      `${ld}/if${rd}`, 
-      `${ld}$stc_cookie_length=strlen($stc_ls_cookie)${rd}`, 
-      `${ld}$stc_ls_cookies=[]${rd}`, 
-      `${ld}$index=0${rd}`, 
-      `${ld}while $index < $stc_cookie_length${rd}`, 
-      `${ld}$stc_ls_cookies[$stc_ls_cookie[$index]]=$stc_ls_cookie[$index+1]${rd}`, 
-      `${ld}$index=$index+2${rd}`, 
-      `${ld}/while${rd}`,
+      `${ld} var stc_ls_cookie = http.cookie("${lsCookie}");`,
+      `var stc_cookie_length = stc_ls_cookie.length;`,
+      `var stc_ls_cookies = {};`,
+      `for(var i = 0; i < stc_cookie_length;i += 2) {`,
+      `stc_ls_cookies[stc_ls_cookie[i]] = stc_ls_cookie[i+1];`,
+      `} ${rd}`,
     ];
 
     return content.join('');
   }
 
   getLsConditionCode(lsValue) {
-    let { ld, rd } = this.config.tpl;
+    let ld = this.config.tpl.ld[0];
+    let rd = this.config.tpl.rd[0];
 
     let data = {};
 
-    data['if'] = `${ld}if isset($stc_ls_config["${lsValue}"]) && isset($stc_ls_cookies[$stc_ls_config["${lsValue}"]["key"]]) && $stc_ls_config["${lsValue}"]["version"] === $stc_ls_cookies[$stc_ls_config["${lsValue}"]["key"]]${rd}`;
-    data['else'] = `${ld}else${rd}`;
-    data['end'] = `${ld}/if${rd}`;
-    data['key'] = `${ld}$stc_ls_config["${lsValue}"]["key"]${rd}`;
-    data['version'] = `${ld}$stc_ls_config["${lsValue}"]["version"]${rd}`;
+    data['if'] = `${ld} if(stc_ls_config["${lsValue}"] && stc_ls_cookies[stc_ls_config["${lsValue}"].key] && stc_ls_config["${lsValue}"].version === stc_ls_cookies[stc_ls_config["${lsValue}"].key]) { ${rd}`;
+    data['else'] = `${ld} } else { ${rd}`;
+    data['end'] = `${ld} } ${rd}`;
+    data['key'] = `${ld} -stc_ls_config["${lsValue}"]["key"] ${rd}`;
+    data['version'] = `${ld} -stc_ls_config["${lsValue}"]["version"] ${rd}`;
 
     return data;
   }
